e0d0077b7deb136ad5d66a201826bba46ab1f860
M Hapiz Maulana R <mhapizmaulanar@gmail.com>
2025-05-27 13:49:53 +0000

    pak sudah di update

---
+++ drink.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.myswiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
